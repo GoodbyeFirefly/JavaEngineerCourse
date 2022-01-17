16805c1adf92580ec38dd7ba35673825e790eec4
GoodbyeFirefly <1961192090@qq.com>
2022-01-17 21:17:11 +0800

    spring整合mybatis，修改配置及测试方法
    添加日志文件配置；
    修改测试方法，sqlSession的创建交给spring，不需要借助MybatisUtil中的getSession方法，比如测试方法AdminMapperTest；
    spring整合mybatis后，不需要显式的关闭sqlSession；
    测试方法采用@RunWith和@ContextConfiguration实现；

--- TaskCards/Chapter11/EMS_SSM/src/test/java/DaoTest.java
+++ TaskCards/Chapter11/EMS_SSM/src/test/java/DaoTest.java
@@ -3,19 +3,25 @@ import com.xxy.service.AdminService;
 import com.xxy.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class DaoTest {
+    @Resource
+    private AdminMapper adminMapper;
 
     @Test
     public void AdminMapperTest() {
-        SqlSession sqlSession = MybatisUtil.getSqlSession();
-        AdminMapper adminMapper = sqlSession.getMapper(AdminMapper.class);
+
         String login = adminMapper.login("admin", "1234");
         if (login == null) System.out.println("false");
         else System.out.println("true");
-        sqlSession.close();
     }
 
     @Test
