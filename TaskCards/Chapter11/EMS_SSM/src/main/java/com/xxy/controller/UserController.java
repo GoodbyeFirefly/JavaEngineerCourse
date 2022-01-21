91ac15fe1c5c52947f56d17b4c88e36d4fcb5ee7
GoodbyeFirefly <1961192090@qq.com>
2022-01-21 09:40:57 +0800

    测试完成ExpressController中的功能

--- TaskCards/Chapter11/EMS_SSM/src/main/java/com/xxy/controller/UserController.java
+++ TaskCards/Chapter11/EMS_SSM/src/main/java/com/xxy/controller/UserController.java
@@ -1,104 +1,122 @@
 package com.xxy.controller;
 
-//import com.xxy.bean.BootstrapTableUser;
-//import com.xxy.bean.Message;
-//import com.xxy.bean.ResultData;
-//import com.xxy.bean.User;
-//import com.xxy.mvc.ResponseBody;
-//import com.xxy.service.UserService;
-//import com.xxy.util.DateFormatUtil;
-//import com.xxy.util.JSONUtil;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.util.ArrayList;
-//import java.util.List;
+import com.xxy.pojo.BootstrapTableUser;
+import com.xxy.pojo.Message;
+import com.xxy.pojo.ResultData;
+import com.xxy.pojo.User;
+import com.xxy.service.UserService;
+import com.xxy.util.DateFormatUtil;
+import com.xxy.util.JsonUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+    @Resource
+    private UserService userService;
+    private User user;
 
-//    @ResponseBody("/user/list.do")
-//    public String list(HttpServletRequest req, HttpServletResponse resp) {
-//        int offset = Integer.parseInt(req.getParameter("offset"));
-//        int pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
-//        List<User> list = UserService.findAll(true, offset, pageNumber);
-//        List<BootstrapTableUser> list2 = new ArrayList<>();
-//        for (User u : list) {
-//            String registertime = DateFormatUtil.format(u.getRegistertime());
-//            String logintime = DateFormatUtil.format(u.getLogintime());
-//            list2.add(new BootstrapTableUser(u.getNumber(), u.getUsername(), u.getUserphone(), u.getIdcard(), u.getPassword(), registertime, logintime));
-//        }
-//        int total = UserService.getTotal();
-//        ResultData<BootstrapTableUser> data = new ResultData();
-//        data.setTotal(total);
-//        data.setRows(list2);
-//        return JSONUtil.toJSON(data);
-//    }
-//
-//    @ResponseBody("/user/insert.do")
-//    public String insert(HttpServletRequest req, HttpServletResponse resp) {
-//        String username = req.getParameter("username");
-//        String userphone = req.getParameter("userphone");
-//        String idcard = req.getParameter("idcard");
-//        String password = req.getParameter("password");
-//        Boolean result = UserService.insert(new User(username, userphone, idcard, password));
-//        Message msg = new Message();
-//        if (result) {
-//            msg.setStatus(0);
-//            msg.setResult("插入成功");
-//        } else {
-//            msg.setStatus(-1);
-//            msg.setResult("插入失败");
-//        }
-//        return JSONUtil.toJSON(msg);
-//    }
-//
-//    @ResponseBody("/user/find.do")
-//    public String find(HttpServletRequest req, HttpServletResponse resp) {
-//        String userphone = req.getParameter("userphone");
-//        User user = UserService.findByPhone(userphone);
-//        Message msg = new Message();
-//        if (user != null) {
-//            msg.setStatus(0);
-//            msg.setResult("查找成功");
-//        } else {
-//            msg.setStatus(-1);
-//            msg.setResult("查找失败");
-//        }
-//        msg.setData(user);
-//        return JSONUtil.toJSON(msg);
-//    }
-//
-//    @ResponseBody("/user/update.do")
-//    public String update(HttpServletRequest req, HttpServletResponse response) {
-//        String username = req.getParameter("username");
-//        String userphone = req.getParameter("userphone");
-//        String idcard = req.getParameter("idcard");
-//        String password = req.getParameter("password");
-//        int number = Integer.parseInt(req.getParameter("number"));
-//        Boolean result = UserService.update(new User(number, username, userphone, idcard, password));
-//        Message msg = new Message();
-//        if (result) {
-//            msg.setStatus(0);
-//            msg.setResult("修改成功");
-//        } else {
-//            msg.setStatus(-1);
-//            msg.setResult("修改失败");
-//        }
-//        return JSONUtil.toJSON(msg);
-//    }
-//
-//    @ResponseBody("/user/delete.do")
-//    public String delete(HttpServletRequest req, HttpServletResponse response) {
-//        String number = req.getParameter("number");
-//        Boolean result = UserService.delete(number);
-//        Message msg = new Message();
-//        if (result) {
-//            msg.setStatus(0);
-//            msg.setResult("删除成功");
-//        } else {
-//            msg.setStatus(-1);
-//            msg.setResult("删除失败");
-//        }
-//        return JSONUtil.toJSON(msg);
-//    }
+    @ResponseBody
+    @RequestMapping("list.do")
+    public String list(HttpServletRequest req, HttpServletResponse resp) {
+        int offset = Integer.parseInt(req.getParameter("offset"));
+        int pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
+        List<User> list = userService.findAll(true, offset, pageNumber);
+        List<BootstrapTableUser> list2 = new ArrayList<>();
+        for (User u : list) {
+            String registertime = DateFormatUtil.format(u.getRegistertime());
+            String logintime = DateFormatUtil.format(u.getLogintime());
+            list2.add(new BootstrapTableUser(u.getNumber(), u.getUsername(), u.getUserphone(), u.getIdcard(), u.getPassword(), registertime, logintime));
+        }
+        int total = userService.getTotal();
+        ResultData<BootstrapTableUser> data = new ResultData();
+        data.setTotal(total);
+        data.setRows(list2);
+        return JsonUtil.toJson(data);
+    }
+
+    @ResponseBody
+    @RequestMapping("insert.do")
+    public String insert(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        String userphone = req.getParameter("userphone");
+        String idcard = req.getParameter("idcard");
+        String password = req.getParameter("password");
+        Boolean result = userService.insert(new User(username, userphone, idcard, password));
+        Message msg = new Message();
+        if (result) {
+            msg.setStatus(0);
+            msg.setResult("插入成功");
+        } else {
+            msg.setStatus(-1);
+            msg.setResult("插入失败");
+        }
+        return JsonUtil.toJson(msg);
+    }
+
+    @ResponseBody
+    @RequestMapping("find.do")
+    public String find(HttpServletRequest req, HttpServletResponse resp) {
+        String userphone = req.getParameter("userphone");
+        user = userService.findByPhone(userphone);
+        Message msg = new Message();
+        if (user != null) {
+            msg.setStatus(0);
+            msg.setResult("查找成功");
+        } else {
+            msg.setStatus(-1);
+            msg.setResult("查找失败");
+        }
+        msg.setData(user);
+        return JsonUtil.toJson(msg);
+    }
+
+    @ResponseBody
+    @RequestMapping("update.do")
+    public String update(HttpServletRequest req, HttpServletResponse response) {
+        String username = req.getParameter("username");
+        String userphone = req.getParameter("userphone");
+        String idcard = req.getParameter("idcard");
+        String password = req.getParameter("password");
+        int number = Integer.parseInt(req.getParameter("number"));
+        user.setUsername(username);
+        user.setUserphone(userphone);
+        user.setIdcard(idcard);
+        user.setPassword(password);
+        user.setNumber(number);
+        Boolean result = userService.update(user);
+        Message msg = new Message();
+        if (result) {
+            msg.setStatus(0);
+            msg.setResult("修改成功");
+        } else {
+            msg.setStatus(-1);
+            msg.setResult("修改失败");
+        }
+        return JsonUtil.toJson(msg);
+    }
+
+    @ResponseBody
+    @RequestMapping("delete.do")
+    public String delete(HttpServletRequest req, HttpServletResponse response) {
+        String number = req.getParameter("number");
+        Boolean result = userService.delete(number);
+        Message msg = new Message();
+        if (result) {
+            msg.setStatus(0);
+            msg.setResult("删除成功");
+        } else {
+            msg.setStatus(-1);
+            msg.setResult("删除失败");
+        }
+        return JsonUtil.toJson(msg);
+    }
 }
