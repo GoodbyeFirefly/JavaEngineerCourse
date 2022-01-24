4529e2b92eb09c5f584c23390ffc50b94b9c54a8
GoodbyeFirefly <1961192090@qq.com>
2022-01-24 17:41:00 +0800

    添加映射接口文件

---
+++ TaskCards/Chapter12/BillManage/src/main/java/com/xxy/mapper/BillMapper.java
@@ -0,0 +1,7 @@
+package com.xxy.mapper;
+
+import com.xxy.entity.Bill;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BillMapper extends Mapper<Bill> {
+}
---
+++ TaskCards/Chapter12/BillManage/src/main/java/com/xxy/mapper/TypeMapper.java
@@ -0,0 +1,7 @@
+package com.xxy.mapper;
+
+import com.xxy.entity.BillType;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TypeMapper extends Mapper<BillType> {
+}
