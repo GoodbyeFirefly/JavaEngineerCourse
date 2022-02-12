cfee43c6006a501c1cb86f8e9fc44eaf7585962b
GoodbyeFirefly <1961192090@qq.com>
2022-02-12 18:17:05 +0800

    完成分类管理部分页面逻辑内容
    fixed: 部分请求（比如/category/toadd）可能出现500错误，可以删除target重新编译试试；

--- TaskCards/Chapter12/TravelMP/src/main/java/com/xxy/controller/CategoryController.java
+++ TaskCards/Chapter12/TravelMP/src/main/java/com/xxy/controller/CategoryController.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin/category")
+@RequestMapping("/admin/category/")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
@@ -50,6 +51,19 @@ public class CategoryController {
     @RequestMapping("delete/{id}")
     public String delete(@PathVariable("id") Integer id) {
         categoryService.delete(id);
+        return "redirect:/admin/category/list";
+    }
+
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
+    @RequestMapping("delete")
+    public String batchDelete(@RequestParam("ids") Integer[] ids) {
+        for (Integer id : ids) {
+            categoryService.delete(id);
+        }
         return "redirect:/admin/category/list";
     }
 
